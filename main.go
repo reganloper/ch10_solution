e13b3d4e35ea48a78cec879aa1cd0f47f670f4ab
Regan Loper <regan.loper@shipt.com>
2023-12-21 13:01:35 -0600

    added godoc comments

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
+// Package ch10_solution provides utility functions to
+// add two numbers together.
 package ch10_solution
 
+// Add takes two integers as parameters and returns the sum
+// of those two integers, as demonstrated in https://www.mathsisfun.com/numbers/addition.html.
 func Add(val1, val2 int) int {
 	return val1 + val2
 }
